3039a6993e73881263b6044d287f720b7e19e4d2
UP2040289 <91467549+UP2040289@users.noreply.github.com>
2023-02-09 22:49:44 +0000

    init

---
+++ app/src/main/java/com/iax3m/bluetoothtracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.iax3m.bluetoothtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
