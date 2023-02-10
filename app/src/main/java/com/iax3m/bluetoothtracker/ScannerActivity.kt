e3e19b0f949a7da6a2fde49cad7826c45ca6e299
Issac <issaca32@gmail.com>
2023-02-10 00:36:22 +0000

    plans activies hierarchy

---
+++ app/src/main/java/com/iax3m/bluetoothtracker/ScannerActivity.kt
@@ -0,0 +1,11 @@
+package com.iax3m.bluetoothtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanner)
+    }
+}
