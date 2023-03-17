cdf60c4a78edc92d376723029259f0e4767aba7e
Issac <issaca32@gmail.com>
2023-03-17 21:34:44 +0000

    implements start page

--- app/src/main/java/com/iax3m/bluetoothtracker/MainActivity.kt
+++ app/src/main/java/com/iax3m/bluetoothtracker/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.iax3m.bluetoothtracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.iax3m.bluetoothtracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var mainBtnStart:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view:View = binding.root
+        setContentView(view)
+        mainBtnStart = binding.mainBtnStart
+        mainBtnStart.setOnClickListener(){
+            val intent = Intent(this, DeviceListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
