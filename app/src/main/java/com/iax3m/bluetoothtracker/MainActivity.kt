3fe7f75b27827586ccb0ed834374cff33f61590f
Issac <issaca32@gmail.com>
2023-03-18 15:14:24 +0000

    shows ad after start btn pressed

--- app/src/main/java/com/iax3m/bluetoothtracker/MainActivity.kt
+++ app/src/main/java/com/iax3m/bluetoothtracker/MainActivity.kt
@@ -1,25 +1,64 @@
 package com.iax3m.bluetoothtracker
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.iax3m.bluetoothtracker.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     lateinit var mainBtnStart:Button
+
+    private var mInterstitialAd: InterstitialAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view:View = binding.root
         setContentView(view)
+
+        getAdInterstitionalMainStart()
+
+        MobileAds.initialize(this) { }
         mainBtnStart = binding.mainBtnStart
         mainBtnStart.setOnClickListener(){
-            val intent = Intent(this, DeviceListActivity::class.java)
-            startActivity(intent)
+
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(this)
+            }
+
+        }
+    }
+
+    fun getAdInterstitionalMainStart(){
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(this,BuildConfig.AD_MAIN_ACTIVITY_INTERSTITIAL, adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                mInterstitialAd = interstitialAd
+            }
+        })
+
+        mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+
+            override fun onAdDismissedFullScreenContent() {
+                mInterstitialAd = null
+            }
+
+            override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                mInterstitialAd = null
+            }
+
         }
     }
 }
