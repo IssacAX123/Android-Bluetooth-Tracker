dd1c36a86f6838d62485f3819ef16ec5c56f8cb3
Issac <issaca32@gmail.com>
2023-03-26 16:41:58 +0100

    Bluetooth utils added

---
+++ app/src/main/java/com/iax3m/bluetoothtracker/util/bluetooth/BluetoothReciever.kt
@@ -0,0 +1,27 @@
+package com.iax3m.bluetoothtracker.util.bluetooth
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BluetoothReciever(
+    private val onDeviceFound: (android.bluetooth.BluetoothDevice) -> Unit
+): BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when(intent?.action){
+            android.bluetooth.BluetoothDevice.ACTION_FOUND ->{
+                val device = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    intent.getParcelableExtra(
+                        android.bluetooth.BluetoothDevice.EXTRA_NAME,
+                        android.bluetooth.BluetoothDevice::class.java
+                    )
+                } else {
+                    intent.getParcelableExtra(android.bluetooth.BluetoothDevice.EXTRA_NAME)
+                }
+                device?.let(onDeviceFound)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/iax3m/bluetoothtracker/util/bluetooth/BluetoothUtil.kt
@@ -0,0 +1,67 @@
+package com.iax3m.bluetoothtracker.util.bluetooth
+
+import android.Manifest
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.IntentFilter
+import kotlin.collections.emptyList
+import android.content.pm.PackageManager
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
+class BluetoothUtil(var context: Context) {
+    private val _devices= MutableStateFlow<List<BluetoothDevice>>(emptyList())
+    val devices: StateFlow<List<BluetoothDevice>>
+        get() = _devices.asStateFlow()
+
+    private val bluetoothManager by lazy {
+        context.getSystemService(BluetoothManager::class.java)
+    }
+
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val bluetoothReciever = BluetoothReciever{device ->
+        _devices.update {devices ->
+            val newDevice = BluetoothDevice(device.name, device.address)
+            if (newDevice in devices) devices else devices+newDevice
+
+        }
+
+    }
+
+
+    fun startSearch(){
+        if(!hasPermission(Manifest.permission.BLUETOOTH_SCAN)){
+            return
+        }
+        context.registerReceiver(
+            bluetoothReciever,
+            IntentFilter(android.bluetooth.BluetoothDevice.ACTION_FOUND)
+        )
+        bluetoothAdapter?.startDiscovery()
+
+    }
+
+    fun stopSearch(){
+        if(!hasPermission(Manifest.permission.BLUETOOTH_SCAN)){
+            return
+        }
+        bluetoothAdapter?.cancelDiscovery()
+
+    }
+
+    fun relase(){
+        context.unregisterReceiver(bluetoothReciever)
+
+    }
+
+    private fun hasPermission(permission: String): Boolean {
+        return context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+}
